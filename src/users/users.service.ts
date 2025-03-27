cefd2ececaa6c1ff248a8e687ea2d38d6e21dc99
Franck <perso@franckdemoute.fr>
2025-03-27 16:48:43 +0100

    feat: NestJS providers introduction

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private readonly users: User[] = [];
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    this.users.push(createUserDto);
+
+    return createUserDto;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
