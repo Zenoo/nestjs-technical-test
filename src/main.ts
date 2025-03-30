97f11b7c2a9f38071dbb850b718c6bb5df96ac27
Franck <perso@franckdemoute.fr>
2025-03-30 12:38:46 +0200

    feat: Empty Swagger doc

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,21 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Swagger setup
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('NEstJS Technical Test API')
+      .setDescription(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      )
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build(),
+  );
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(configService.get('PORT') ?? 3000);
 }
 
